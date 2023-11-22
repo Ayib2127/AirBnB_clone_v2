98d51be9afa4a4f6c988607f42acb577b812cfde
Silicon50 <ndukwechukwuaja@gmail.com>
2023-11-22 15:45:30 +0100

    mysql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,9 +2,9 @@
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
 --create a new user hbnb_test
-CREATE USER IF NOT EXISTS hbnb_test IDENTIFIED BY 'hbnb_test_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
 --granting privileges to user
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO hbnb_test@localhost;
-GRANT SELECT ON performance_schema.* TO hbnb_test@localhost;
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;

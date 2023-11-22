f57a973597c7a577666f69bffed09d89d7463e63
Silicon50 <ndukwechukwuaja@gmail.com>
2023-11-22 15:49:13 +0100

    mysql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,10 +1,10 @@
---create a schema hbnb_test_db
+-- A script that prepares a MySQL server for the project
+-- Create database if not exists
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-
---create a new user hbnb_test
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-
---granting privileges to user
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+-- Create user if not exists
+CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
+-- Grant privileges to user
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO hbnb_test@localhost;
+GRANT SELECT ON performance_schema.* TO hbnb_test@localhost;
+-- Flush privileges
 FLUSH PRIVILEGES;

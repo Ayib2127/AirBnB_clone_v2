a41697b4f45c604eec4a34eab86d399e26eeba42
Silicon50 <ndukwechukwuaja@gmail.com>
2023-11-22 15:51:16 +0100

    mysql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,6 @@
--- A script that prepares a MySQL server for the project
--- Create database if not exists
+-- Create database hbnb_test_db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
--- Create user if not exists
+-- Create user bnb_test
 CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
 -- Grant privileges to user
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO hbnb_test@localhost;
